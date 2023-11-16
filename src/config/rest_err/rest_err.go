a91d5d070c244a8d2af6b839b321748d5f1e1a9e
rafa <rafaeldutraribas@gmail.com>
2023-11-16 17:42:41 -0300

    struct de errors e dot env

---
+++ src/config/rest_err/rest_err.go
@@ -0,0 +1,33 @@
+package resterr
+
+import "net/http"
+
+type RestErr struct {
+	Message string `json:"message"`
+	Err     string `json:"error"`
+	Code    int    `json:"code"`
+}
+
+func (r *RestErr) Error() string {
+	return r.Message
+}
+
+func NewRestErr(message string, err string, code int) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+	}
+}
+
+func NewBadResquestErr(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+func NewInternalServerErr() *RestErr {
+	return &RestErr{Message: "Ocorreu um erro inesperado", Err: "internal server error", Code: http.StatusInternalServerError}
+}
