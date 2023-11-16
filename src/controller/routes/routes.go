8a001afa95c0200ad6474faddf9db7a0b263bf3f
rafa <rafaeldutraribas@gmail.com>
2023-11-16 19:26:24 -0300

    renomeia package user_controller para controller

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -1,14 +1,14 @@
 package routes
 
 import (
-	user_controller "github.com/RafaDRF/crud-go/src/controller"
+	controller "github.com/RafaDRF/crud-go/src/controller"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(r *gin.RouterGroup) {
-	r.GET("/userById/:userId", user_controller.FindUserById)
-	r.GET("/userByEmail/:userEmail", user_controller.FindUserByEmail)
-	r.POST("/user", user_controller.CreateUser)
-	r.PUT("/user/:userId", user_controller.UpdateUser)
-	r.DELETE("/user/:userId", user_controller.DeleteUser)
+	r.GET("/userById/:userId", controller.FindUserById)
+	r.GET("/userByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/user", controller.CreateUser)
+	r.PUT("/user/:userId", controller.UpdateUser)
+	r.DELETE("/user/:userId", controller.DeleteUser)
 }
--- src/controller/userController.go
+++ src/controller/userController.go
@@ -1,8 +1,20 @@
-package user_controller
+package controller
 
 import (
+	"github.com/RafaDRF/crud-go/src/config/rest_err"
+	"github.com/RafaDRF/crud-go/src/model/request"
 	"github.com/gin-gonic/gin"
 )
+
+func CreateUser(c *gin.Context) {
+	var userReq request.UserRequest
+
+	if err := c.ShouldBindJSON(&userReq); err != nil {
+		restErr := rest_err.NewBadResquestErr(err.Error())
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+}
 
 func FindUserById(c *gin.Context) {
 
@@ -10,9 +22,6 @@ func FindUserById(c *gin.Context) {
 
 func FindUserByEmail(c *gin.Context) {
 
-}
-
-func CreateUser(c *gin.Context) {
 }
 
 func UpdateUser(c *gin.Context) {
